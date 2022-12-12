b1b6660a428129e57f612b5e8e7a749247cfe847
Gwen Guérin <gwenael.guerin@protonmail.com>
2022-12-12 17:43:51 +0100

    gwen back first

---
+++ models/users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  username: String,
+  email: String,
+  password: String,
+  token: String,
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch')
+require('../models/connection');
+const User = require('../models/users');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.post('/signup', (req, res) => {
+
+  const { firstname, lastname, username, email, password } = req.body
+
+  const hash = bcrypt.hashSync(password, 10);
+
+  // Check if the user has not already been registered
+  if (username && password) {
+    User.findOne({ username: username }).then((data) => {
+      if (data === null) {
+        const newUser = new User({
+          firstname,
+          lastname,
+          username,
+          email,
+          password: hash,
+          token: uid2(32),
+        });
+
+        newUser.save().then((newUser) => {
+          res.json({ result: true, user: newUser });
+        });
+      } else {
+        // User already exists in database
+        res.json({ result: false, error: 'User already exists' });
+      }
+    });
+  } else {
+    res.json({ result: false, error: 'Missing or empty fields' });
+  }
+});
+
+router.post('/signin', (req, res) => {
+  const { firstname, lastname, username, email, password } = req.body
+
+  if (username && password) {
+    User.findOne({ username: username }).then((data) => {
+      if (data === null) {
+        res.json({ result: false, error: 'User not found' });
+      } else {
+        res.json({ result: true , user: data});
+      }
+    });
+  } else {
+    res.json({ result: false, error: 'Missing or empty fields' });
+  }
+});
+
 
 module.exports = router;
