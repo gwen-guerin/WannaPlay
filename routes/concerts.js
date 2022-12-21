62e0e5896f598769c28de1d3684772e7b2e488fb
Gwen Guérin <gwenael.guerin@protonmail.com>
2022-12-21 16:14:14 +0100

    gwen back

--- routes/concerts.js
+++ routes/concerts.js
@@ -4,45 +4,45 @@ const Concert = require('../models/concerts');
 const fetch = require('node-fetch');
 require('../models/connection');
 
-router.get('/', (req, res) => {
-  Concert.find().then((data) => {
-    console.log('concerts', data);
-    if (data) {
-      data.map((concert, i) => {
-        res.json({
-          result: true,
-          concert: {
-            eventName: data[i].eventName,
-            date: data[i].date,
-            style: data[i].style,
-            place: data[i].place,
-          },
-        });
-      });
-    }
-  });
-});
+// router.get('/', (req, res) => {
+//   Concert.find().then((data) => {
+//     console.log('concerts', data);
+//     if (data) {
+//       data.map((concert, i) => {
+//         res.json({
+//           result: true,
+//           concert: {
+//             eventName: data[i].eventName,
+//             date: data[i].date,
+//             style: data[i].style,
+//             place: data[i].place,
+//           },
+//         });
+//       });
+//     }
+//   });
+// });
 
 
 
-router.post("/createConcert", (req, res) => {
-  const { eventName, date, type, places } = req.body;
+// router.post("/createConcert", (req, res) => {
+//   const { eventName, date, type, places } = req.body;
 
-  Concert.findOne({ eventName: eventName }).then((data) => {
-    if (data === null) {
-      const newConcert = new Concert({
-        eventName: eventName,
-        date: date,
-        type: type,
-        places: places,
-      });
-      newConcert.save().then((data) => {
-        res.json({ result: true, eventName: data });
-      });
-    } else {
-      res.json({ result: false, error: 'Concert already exists' });
-    }
-  });
-});
+//   Concert.findOne({ eventName: eventName }).then((data) => {
+//     if (data === null) {
+//       const newConcert = new Concert({
+//         eventName: eventName,
+//         date: date,
+//         type: type,
+//         places: places,
+//       });
+//       newConcert.save().then((data) => {
+//         res.json({ result: true, eventName: data });
+//       });
+//     } else {
+//       res.json({ result: false, error: 'Concert already exists' });
+//     }
+//   });
+// });
 
 module.exports = router;
